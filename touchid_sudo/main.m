bb9d4b2003316695e655c88cc3a80bd4c83d067a
Clément Padovani <ClementPadovani@users.noreply.github.com>
2017-07-15 14:23:59 +0200

    Initial commit

---
+++ touchid_sudo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  touchid_sudo
+//
+//  Created by Clément Padovani on 7/15/17.
+//  Copyright © 2017 Clément Padovani. All rights reserved.
+//
+
+@import Foundation;
+
+int main(int argc, const char * argv[])
+{
+    return 0;
+}
